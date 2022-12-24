0b217e153723ad5cc1dc03656c85d404fd28643f
wellsant7778 <89748778+wellsant7778@users.noreply.github.com>
2022-12-23 19:34:59 -0600

    re-oganizing project files, adding pg-csv.sh

--- apps/samples/pg-csv.py
+++ apps/samples/pg-csv.py
@@ -1,13 +1,22 @@
+
+"""
+pg-csv.py: This ETL job takes a small flat file, and loads it into the postgres
+database. This file is a modified version of the main.py file found within 
+mvillarealb's https://github.com/mvillarrealb/docker-spark-cluster/.  It serves
+as a baseline example of a spark application.  Use pg.sh to run this job.
+"""
+
+
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col,date_format
+from pyspark.sql.functions import col, date_format
 
 def init_spark():
   sql = SparkSession.builder\
-    .appName("trip-app")\
-    .config("spark.jars", "/opt/spark-apps/postgresql-42.2.22.jar")\
-    .getOrCreate()
+        .appName("trip-app")\
+        .config("spark.jars", "/opt/spark-apps/postgresql-42.2.22.jar")\
+        .getOrCreate()
   sc = sql.sparkContext
-  return sql,sc
+  return sql, sc
 
 def main():
   url = "jdbc:postgresql://demo-database:5432/postgres"
@@ -17,7 +26,7 @@ def main():
     "driver": "org.postgresql.Driver"
   }
   file = "/opt/spark-data/us-500.csv"
-  sql,sc = init_spark()
+  sql, sc = init_spark()
 
   df = sql.read.load(file,format = "csv", inferSchema="true", sep=",", header="true")
   
