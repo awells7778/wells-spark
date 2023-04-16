b28936fc66622c893fe3f27aea216ce39f24d6f3
wellsant7778 <89748778+wellsant7778@users.noreply.github.com>
2023-04-16 18:04:54 -0500

    added grand exchange datalake utils

--- spark/apps/datalake/osrs/utils/grand_exchange.py
+++ spark/apps/datalake/osrs/utils/grand_exchange.py
@@ -16,23 +16,23 @@ def flatten(schema, prefix=None):
     return fields
 
 ge_item_schema = StructType([
-    StructField('current_price', StringType(), True),#0
-    StructField('current_trend', StringType(), True),#1
-    StructField('day180_change', StringType(), True),#2
-    StructField('day180_trend', StringType(), True),#3
-    StructField('day30_change', StringType(), True),#4
-    StructField('day30_trend', StringType(), True),#5
-    StructField('day90_change', StringType(), True),#6
-    StructField('day90_trend', StringType(), True),#7
-    StructField('description', StringType(), True),#8
-    StructField('icon', StringType(), True),#9
-    StructField('icon_large', StringType(), True),#10
-    StructField('id', LongType(), True),#11
-    StructField('members', StringType(), True),#12
-    StructField('name', StringType(), True),#13
-    StructField('today_price', StringType(), True),#14
-    StructField('today_trend', StringType(), True),#15
-    StructField('type', StringType(), True),#16
-    StructField('typeIcon', StringType(), True)#17
+    StructField('current_price', StringType(), True), #0
+    StructField('current_trend', StringType(), True), #1
+    StructField('day180_change', StringType(), True), #2
+    StructField('day180_trend', StringType(), True),  #3
+    StructField('day30_change', StringType(), True),  #4
+    StructField('day30_trend', StringType(), True),   #5
+    StructField('day90_change', StringType(), True),  #6
+    StructField('day90_trend', StringType(), True),   #7
+    StructField('description', StringType(), True),   #8
+    StructField('icon', StringType(), True),          #9
+    StructField('icon_large', StringType(), True),    #10
+    StructField('id', LongType(), True),              #11
+    StructField('members', StringType(), True),       #12
+    StructField('name', StringType(), True),          #13
+    StructField('today_price', StringType(), True),   #14
+    StructField('today_trend', StringType(), True),   #15
+    StructField('type', StringType(), True),          #16
+    StructField('typeIcon', StringType(), True)       #17
 ])
 
