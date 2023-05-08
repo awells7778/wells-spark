d9f52f2002410a8a326ae885845acc4631682aa6
wellsant7778 <89748778+wellsant7778@users.noreply.github.com>
2023-05-07 20:06:21 -0500

    finished landing/grand_exchange

--- spark/apps/datalake/landing/osrs/grand_exchange.py
+++ spark/apps/datalake/landing/osrs/grand_exchange.py
@@ -1,42 +1,55 @@
 """
-Landing job to extracting Grand Exchange data from the OSRS API.  This job returns a json file
+Landing job to extracting Grand Exchange data from the public OSRS API.  This job returns a json file
 that will be flattened into a parquet file during the staging process.
 """
 
 import requests
 import json
-import configparser
 import os
+import time
+from datetime import datetime
 from string import ascii_lowercase
 
 
 def main():
+    
+    # Configure working directory
+    os.chdir("/opt")
+    
     # Get our combined json response object 
     combined_results = []
-    
-  #  for letter in ascii_lowercase:
-    category = 1
-    page = 1
-        
-    while True:
-        url = f"https://secure.runescape.com/m=itemdb_oldschool/api/catalogue/items.json?category={category}&alpha=a&page={page}"
-        response = requests.get(url)
-        response_data = response.json()
-        
-        if response.status_code != 200:
-            print(f"Request failed with status code {response.status_code}")
-            break
-        
-        if not response_data["items"]:
-            break
-        
-        combined_results.extend(response_data["items"])
-        page += 1
-        print(f"incrementing page number {page}")
-    # and dump it into target json file    
-    with open ("landing_grand_exchange.json", "w") as f:
-        json.dump(combined_results, f)
+
+    for letter in ascii_lowercase:
+        category = 1 # grand exchange items
+        page = 1 # first page only due to request throttle
             
+        url = f"https://secure.runescape.com/m=itemdb_oldschool/api/catalogue/items.json?category={category}&alpha={letter}&page={page}"
+        response = requests.get(url)
+        
+        print(f"incrementing page number {letter} - {page}: response code: {response.status_code}")
+        
+        try :
+            response_data = response.json()
+            
+            if response.status_code != 200:
+                print(f"Request failed with status code {response.status_code}")
+                break
+            
+            if len(response_data) > 0:
+                combined_results.extend(response_data["items"])
+
+            
+        except requests.exceptions.JSONDecodeError as e:
+            print("Error decoding JSON:", e)
+        
+        # API throttles requests at 1 every 5 seconds        
+        time.sleep(5)
+
+    # and dump it into target json file 
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    target_file = f"spark-data/datalake/landing/lnd_grand_exchange_{timestamp}.json"       
+    with open (target_file, "w") as f:
+        json.dump(combined_results, f)
 
 if __name__ == "__main__":
     main()
